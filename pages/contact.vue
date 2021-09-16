af3fcc56d9e8f88be09b242d8fd1c216399b8025
joelsefubolembo <joelsefubolembo@gmail.com>
2021-09-16 19:42:35 +0100

    add pages

---
+++ pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Contact page</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/">contact</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/contact">contact</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
